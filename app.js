16c94340701470bb78e6fc51486f38272c4ca21b
Kais Burwais <kaisburwais@gmail.com>
2026-07-21 23:52:03 +0200

    v4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`Trigger the ArgoCD CD! Version: ${process.env.APP_VERSION || 'v3'}`);
+  res.send(`Trigger the ArgoCD CD! Version: ${process.env.APP_VERSION || 'v4'}`);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
